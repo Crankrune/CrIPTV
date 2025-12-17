7f817b59be55332c0cad629091b1756dba4b94aa
Crankrune <crankrune@gmail.com>
2025-12-17 00:26:01 -0600

    Disabled youtube functions again, not working. Added new playlist to give all possible channels, including the commented urls

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import json
+import re
+from pprint import pprint
 
 from natsort import natsorted
 from ruamel.yaml import YAML
@@ -56,6 +58,43 @@ def generate_iptv_playlists():
     print("Finished IPTV playlist generation.")
 
 
+def generate_full_iptv_playlist():
+    with open("data/iptv_database.json", "r", encoding="utf-8") as f:
+        playlist_data = json.load(f)
+
+    full_playlist_data = []
+    seen_channel_urls = set()
+    for channel in playlist_data:
+        if not len(channel["commented_urls"]):
+            full_playlist_data.append(channel)
+        else:
+            urls = []
+            if channel["url"] not in seen_channel_urls:
+                urls.append(channel["url"])
+                seen_channel_urls.add(channel["url"])
+
+            for commented_url in channel["commented_urls"]:
+                url = re.sub("^#\s*", "", commented_url)
+                if url not in seen_channel_urls:
+                    urls.append(url)
+                    seen_channel_urls.add(url)
+
+            for enum, url in enumerate(urls, start=1):
+                new_channel = channel.copy()
+                new_channel["url"] = url
+                new_channel["tvg-name"] = f"{channel['tvg-name']} ({enum})"
+                new_channel["name"] = f"{channel['name']} ({enum})"
+                new_channel["commented_urls"] = []
+                full_playlist_data.append(new_channel)
+
+    playlist_content = generate_playlist(
+        natsorted(full_playlist_data, key=lambda ch: ch["name"].casefold())
+    )
+    with open("output/playlists/playlist_all.m3u", "w", encoding="utf-8") as f:
+        f.write(playlist_content)
+    print("Generated output/playlists/playlist_all.m3u")
+
+
 def generate_youtube_playlist():
     """
     Updates the YouTube streams and generates the corresponding M3U playlist.
@@ -74,5 +113,6 @@ def generate_youtube_epg():
 if __name__ == "__main__":
     check_streams()
     generate_iptv_playlists()
-    generate_youtube_playlist()
-    generate_youtube_epg()
+    generate_full_iptv_playlist()
+    # generate_youtube_playlist()
+    # generate_youtube_epg()
